38e06f6aa7c5343be1352f5a11c29314c0b0e2c9
Simon-Njoroge <mukirisimon22@gmail.com>
2026-05-13 22:34:57 +0300

    feat: setup ConfigModule and GraphQLModule with Apollo Server integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import {ConfigModule} from '@nestjs/config';
+import {GraphQLModule} from "@nestjs/graphql";
+import {ApolloDriver, ApolloDriverConfig} from "@nestjs/apollo";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
